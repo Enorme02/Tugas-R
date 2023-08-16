7ef758941d451d1da98d46d88a7d618a487bf571
Enorme02 <syahilakbarmuda@gmail.com>
2023-08-16 10:31:20 +0700

    Upload Project Kalkulator Segitiga

---
+++ Tugas-R/java/java.js
@@ -0,0 +1,17 @@
+const tinggi = document.getElementById('tinggi-input')
+const alas = document.getElementById('alas-input')
+const hitung = document.getElementById('hitung')
+const reset = document.getElementById('reset')
+const keliling = document.getElementById('keliling')
+const output = document.getElementById('luas')
+hitung.addEventListener('click', function (){
+    output.innerText = (alas.value * tinggi.value)/2
+})
+reset.addEventListener('click', function (){
+    tinggi.value = null
+    alas.value = null
+    output.value = ''
+})
+keliling.addEventListener('click', function (){
+    
+})
